0035f15cacecbcd4073e888425873919a0a96a8c
David Rusu <davidrusu.me@gmail.com>
2020-12-21 18:48:56 +0400

    Use thiserror to provide users with standards compliant Rust Errors

--- src/bft_membership.rs
+++ src/bft_membership.rs
@@ -2,6 +2,7 @@ use std::collections::{BTreeMap, BTreeSet};
 
 use bincode;
 use serde::{Deserialize, Serialize};
+use thiserror::Error;
 
 use crate::actor::{Actor, Sig, SigningActor};
 use crate::packet::{Packet, Payload};
@@ -130,53 +131,56 @@ impl Vote {
     }
 }
 
-#[derive(Debug)]
+#[derive(Error, Debug)]
 pub enum Error {
+    #[error("Vote has an invalid signature")]
     InvalidSignature,
-    WrongDestination {
-        dest: Actor,
-        actor: Actor,
-    },
-    MembersAtCapacity {
-        members: BTreeSet<Actor>,
-    },
+    #[error("Packet was not destined for this actor: {dest} != {actor}")]
+    WrongDestination { dest: Actor, actor: Actor },
+    #[error(
+        "We can not accept any new join requests, network member size is at capacity: {members:?}"
+    )]
+    MembersAtCapacity { members: BTreeSet<Actor> },
+    #[error(
+        "An existing member `{requester}` can not request to join again. (members: {members:?})"
+    )]
     JoinRequestForExistingMember {
         requester: Actor,
         members: BTreeSet<Actor>,
     },
+    #[error("You must be a member to request to leave ({requester} not in {members:?})")]
     LeaveRequestForNonMember {
         requester: Actor,
         members: BTreeSet<Actor>,
     },
+    #[error("A vote is always for the next generation: vote gen {vote_gen} != {gen} + 1")]
     VoteNotForNextGeneration {
         vote_gen: Generation,
         gen: Generation,
         pending_gen: Generation,
     },
+    #[error("Vote from non member ({voter} not in {members:?})")]
     VoteFromNonMember {
         voter: Actor,
         members: BTreeSet<Actor>,
     },
+    #[error("Voter changed their mind: {reconfigs:?}")]
     VoterChangedMind {
         reconfigs: BTreeSet<(Actor, Reconfig)>,
     },
-    ExistingVoteIncompatibleWithNewVote {
-        vote: Vote,
-        existing_vote: Vote,
-    },
+    #[error("Existing vote {existing_vote:?} not compatible with new vote: {vote:?}")]
+    ExistingVoteIncompatibleWithNewVote { vote: Vote, existing_vote: Vote },
+    #[error("The super majority ballot does not actually have supermajority: {ballot:?} (members: {members:?})")]
     SuperMajorityBallotIsNotSuperMajority {
         ballot: Ballot,
         members: BTreeSet<Actor>,
     },
+    #[error("Invalid generation {0}")]
     InvalidGeneration(Generation),
+    #[error("History contains an invalid vote {0:?}")]
     InvalidVoteInHistory(Vote),
-    Encoding(bincode::Error),
-}
-
-impl From<bincode::Error> for Error {
-    fn from(err: bincode::Error) -> Self {
-        Self::Encoding(err)
-    }
+    #[error("Failed to encode with bincode")]
+    Encoding(#[from] bincode::Error),
 }
 
 impl State {
